f8a621f19644f169c0566318599d60af7126b978
Habib <contact@devhabib.com>
2020-03-07 03:31:51 +0600

    attribute binding in courses.component.ts

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,15 @@ import { Component } from "@angular/core";
 @Component({
   selector: "courses",
   template: `
-    <h2>{{ title }}</h2>
-    // <img src="{{ imgUrl }}" />
     <img [src]="imgUrl" />
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
   `
 })
 export class CoursesComponent {
-  title = "List of Courses";
+  colSpan = "List of Courses";
   imgUrl = "http://lorempixel.com/400/200";
 }
