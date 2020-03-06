3f761140d8b3244b3454db23b22bbe0f7457dd68
Habib <contact@devhabib.com>
2020-03-07 03:00:02 +0600

    Angular Fundamentals course finished from MOSH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CoursesService } from "./courses.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoursesComponent } from "../app/courses.component";
+import { CourseComponent } from "./course/course.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CourseComponent, CoursesComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
