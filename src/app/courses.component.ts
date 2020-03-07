38b098f431a391c6cc5e68f4f010cee9cf4a574e
Habib <contact@devhabib.com>
2020-03-07 15:13:38 +0600

    pipes example one

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,19 @@ import { Component } from "@angular/core";
 @Component({
   selector: "courses",
   template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    {{ course.title | uppercase }}<br />
+    {{ course.rating | number: "2.1-1" }}<br />
+    {{ course.students | number }}<br />
+    {{ course.price | currency: "AUD":true:"3.2-2" }}<br />
+    {{ course.releaseDate | date: "mediumDate" }}<br />
   `
 })
 export class CoursesComponent {
-  email = "me@example.com";
-  onKeyUp() {
-    console.log(this.email);
-  }
+  course = {
+    title: "This is a course",
+    rating: 4.9556,
+    students: 45623,
+    price: 195.36,
+    releaseDate: new Date(2016, 3, 1)
+  };
 }
