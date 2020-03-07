34f5fe91936fbc7f2d6f7d3f8dc9ab567367eeab
Habib <contact@devhabib.com>
2020-03-07 15:30:45 +0600

    making custom pipes, summary.pipe is a custom pipe, implimented on app.module and worked on courses.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SummaryPipe } from "./summary.pipe";
 import { CoursesService } from "./courses.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
@@ -9,7 +10,7 @@ import { CoursesComponent } from "../app/courses.component";
 import { CourseComponent } from "./course/course.component";
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent, CoursesComponent],
+  declarations: [AppComponent, CourseComponent, CoursesComponent, SummaryPipe ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: "courses",
   template: `
-    {{ course.title | uppercase }}<br />
-    {{ course.rating | number: "2.1-1" }}<br />
-    {{ course.students | number }}<br />
-    {{ course.price | currency: "AUD":true:"3.2-2" }}<br />
-    {{ course.releaseDate | date: "mediumDate" }}<br />
+    {{ text | summary }}
   `
 })
 export class CoursesComponent {
-  course = {
-    title: "This is a course",
-    rating: 4.9556,
-    students: 45623,
-    price: 195.36,
-    releaseDate: new Date(2016, 3, 1)
-  };
+  text =
+    "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ";
 }
